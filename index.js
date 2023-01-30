3cf83511d9ca0db415fd2d38aa682155f9872457
NeeshamKalia <neeshamkalia007@gmail.com>
2023-01-30 11:55:10 +0530

    adding routes

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req,res,next) => {
-    console.log("hello from middlewarE");
-    next();
-});
-app.use((req,res,next) => {
-   console.log("hello from another middleware");
-   res.send('<h1>hello from Express</h1>');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use('/admin', adminRoutes);
+
+app.use(shopRoutes);
+app.use((req,res,next) =>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
-
-
-
-
-
 app.listen(3000)

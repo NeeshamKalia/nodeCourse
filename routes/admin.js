763664b3493ffb0d84162bef37d1424807f57159
NeeshamKalia <neeshamkalia007@gmail.com>
2023-01-30 12:34:37 +0530

    added html

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
+const path = require('path');
 const express = require('express')
 const router = express.Router();
 
+const routeDir = require('../util/path')
 
 // admin/add-product (GET)
 router.get('/add-product',(req,res,next) => {
-
-    res.send('<form action="/admin/add-product" method="POST"><input type = "text" name = "title"><button type = "submit">Add Product</button></form>');
+    res.sendFile(path.join(routeDir, 'views', 'add-product.html'));
+    /* res.send('<form action="/admin/add-product" method="POST"><input type = "text" name = "title"><button type = "submit">Add Product</button></form>'); */
  })
- 
+
  // admin/add-product (POST)
  router.post('/add-product', (req,res) => {
-     console.log(req.body);
+     //console.log(req.body);
 
      res.redirect('/');
  })
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
+const path = require('path');
 const express = require('express');
 const router =  express.Router();
 
+const routeDir = require('../util/path')
 
 
 router.get('/', (req, res) => {
-    res.send('<h1>Hello from Express</h1>');
+    //res.send('<h1>Hello from Express</h1>');
+    res.sendFile(path.join(routeDir, 'views', 'shop.html'));
  });
 
  module.exports = router;

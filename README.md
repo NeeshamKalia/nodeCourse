457c515517dcb1c8f4c82e6aeeafec1f8f0c8c9d
NeeshamKalia <neeshamkalia007@gmail.com>
2023-02-03 11:54:20 +0530

    separating html and css folders

--- README.md
+++
@@ -1,3 +0,0 @@
-# nodeCourse
-NodeJs, the complete guide on Udemy
- code and project work based upon the NodeJs course on Udemy.
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,13 @@
 const path = require('path');
+
 const express = require('express');
-const router =  express.Router();
 
-const routeDir = require('../util/path')
+const rootDir = require('../util/path');
 
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    //res.send('<h1>Hello from Express</h1>');
-    res.sendFile(path.join(routeDir, 'views', 'shop.html'));
- });
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
 
- module.exports = router;
+module.exports = router;
